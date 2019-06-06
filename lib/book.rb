c96a4cfaef5324551ad2e7dcaaedc9a3c24780e4
Alex Siler <davidalexsiler@gmail.com>
2019-06-06 15:54:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,5 @@
 class Book
+<<<<<<< HEAD
   attr_accessor :author, :page_count, :genre
   attr_reader :title
   
@@ -9,4 +10,9 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+=======
+  def initialize(title)
+    @title = title
+  end
+>>>>>>> 558917295700b3b78ee1cddd3d17d0d4b81236f8
 end 
